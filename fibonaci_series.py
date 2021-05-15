e0ec01001327472483b747b11c412ee3c0443b6a
niharikasaraswat <82013881+niharikasaraswat@users.noreply.github.com>
2021-05-15 16:10:33 +0530

    Add files via upload

---
+++ fibonaci_series.py
@@ -0,0 +1,18 @@
+# enter series range
+Number = int(input("\nPlease Enter the Range Number: "))
+
+# Initializing First and Second Values of a Series
+i = 0
+First_Value = 0
+Second_Value = 1
+
+# Find & Displaying Fibonacci series
+while i < Number:
+    if i <= 1:
+        Next = i
+    else:
+        Next = First_Value + Second_Value
+        First_Value = Second_Value
+        Second_Value = Next
+    print(Next)
+    i = i + 1
