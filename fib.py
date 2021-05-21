aee2eb96f9eaeb7ac6d8cb8fd0862a47343329fa
niharikasaraswat <82013881+niharikasaraswat@users.noreply.github.com>
2021-05-21 15:45:39 +0530

    Add files via upload

---
+++ fib.py
@@ -0,0 +1,7 @@
+# enter series range
+list1 = [20, -60, 80, 50, -10]
+for num in list1:
+    if num > 1:
+        print('positive numbers in list', num)
+    else:
+        print('negative numbers in list', num)
